a0d8aa3907c3ee34c1c221ec80def9f9e2ab5a30
Richard Neil Ilagan <me@richardneililagan.com>
2016-06-29 18:18:21 +0800

    Adds server connectivity logging

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const config = require('../config')
 
 require('./routes.js').init(app)
 
+console.log(`Attempting server start on port ${ config.port } ...`)
 app.listen(config.port)
 console.log(`App listening on port ${ config.port }.`)
 
